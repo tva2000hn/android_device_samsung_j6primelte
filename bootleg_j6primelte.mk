970d9330cdbcc7cc3063e5527b4189119b3eef1b
tva2000hn <vietanhfat@gmail.com>
2020-04-24 11:00:45 +0700

    Update and rename lineage_j6primelte.mk to bootleg_j6primelte.mk

--- bootleg_j6primelte.mk
+++ bootleg_j6primelte.mk
@@ -17,7 +17,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/bootleggers/config/common_full_phone.mk)
 
 # Inherit from j6primelte device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -28,7 +28,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 PRODUCT_BRAND := samsung
 PRODUCT_DEVICE := j6primelte
 PRODUCT_MANUFACTURER := samsung
-PRODUCT_NAME := lineage_j6primelte
+PRODUCT_NAME := bootleg_j6primelte
 PRODUCT_MODEL := Galaxy J6+
 
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
